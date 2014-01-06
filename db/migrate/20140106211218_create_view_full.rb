86f5074ea3cc36bcecdab6ff1ab3b960ff56caad
Rob Wilson <roobert@gmail.com>
2014-01-06 21:17:19 +0000

    added sql exec

---
+++ db/migrate/20140106211218_create_view_full.rb
@@ -0,0 +1,37 @@
+class CreateViewFull < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE VIEW view_full AS
+        SELECT
+          networks.location AS location,
+          switches.hostname AS switch,
+          ethernet_interfaces.stack_member AS stack_member,
+          ethernet_interfaces.speed AS speed,
+          ethernet_interfaces.port AS port,
+          ethernet_interfaces.description AS port_description,
+          switchports.mode AS mode,
+          switchports.port_type AS port_type,
+          switchports.tagged AS tagged,
+          switchports.acceptable_frame_type AS acceptable_frame_type,
+          switchports.permission AS permission,
+          vlans.vlan AS vlan,
+          vlan_interfaces.description AS vlan_description,
+          macs.mac AS mac
+        FROM
+          switches
+        INNER JOIN networks              ON switches.network_id                  = networks.id
+        INNER JOIN ethernet_interfaces   ON ethernet_interfaces.switch_id        = switches.id
+        INNER JOIN switchports           ON switchports.ethernet_interface_id    = ethernet_interfaces.id
+        INNER JOIN vlans                 ON vlans.switchport_id                  = switchports.id
+        INNER JOIN vlan_interfaces       ON vlan_interfaces.vlan                 = vlans.vlan
+        INNER JOIN bridge_address_tables ON bridge_address_tables.switch_id      = switches.id
+        INNER JOIN bridge_vlans          ON bridge_vlans.bridge_address_table_id = bridge_address_tables.id
+        INNER JOIN macs                  ON macs.bridge_vlan_id                  = bridge_vlans.id
+    SQL
+
+  end
+
+  def down
+    drop_table :view_full
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,88 +1,71 @@
-#!/usr/bin/env ruby
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define do
-  create_table :networks do |t|
-    t.column :location, :string
+ActiveRecord::Schema.define(version: 20140106211218) do
+
+  create_table "bridge_address_tables", force: true do |t|
+    t.string  "identifier"
+    t.integer "port"
+    t.integer "stack_member"
+    t.string  "speed"
+    t.integer "switch_id"
   end
 
-  create_table :switches do |t|
-    t.column :hostname, :string
-    t.belongs_to :network
+  create_table "bridge_vlans", force: true do |t|
+    t.integer "vlan"
+    t.integer "bridge_address_table_id"
   end
 
-  create_table :vlan_interfaces do |t|
-    t.column :description, :string
-    t.column :vlan, :int
-    t.belongs_to :switch
+  create_table "ethernet_interfaces", force: true do |t|
+    t.string  "description"
+    t.integer "port"
+    t.integer "stack_member"
+    t.string  "speed"
+    t.integer "switch_id"
   end
 
-  create_table :ethernet_interfaces do |t|
-    t.column :description, :string
-    t.column :port, :int
-    t.column :stack_member, :int
-    t.column :speed, :string
-    t.belongs_to :switch
+  create_table "macs", force: true do |t|
+    t.string  "mac"
+    t.string  "mode"
+    t.integer "bridge_vlan_id"
   end
 
-  create_table :switchports do |t|
-    t.column :mode, :string
-    t.column :port_type, :string
-    t.column :tagged, :boolean
-    t.column :acceptable_frame_type, :string
-    t.column :permission, :string
-    t.belongs_to :ethernet_interface
+  create_table "networks", force: true do |t|
+    t.string "location"
   end
 
-  create_table :vlans do |t|
-    t.column :vlan, :int
-    t.belongs_to :switchport
+  create_table "switches", force: true do |t|
+    t.string  "hostname"
+    t.integer "network_id"
   end
 
-  create_table :bridge_address_tables do |t|
-    t.column :identifier, :string
-    t.column :port, :int
-    t.column :stack_member, :int
-    t.column :speed, :string
-    t.belongs_to :switch
+  create_table "switchports", force: true do |t|
+    t.string  "mode"
+    t.string  "port_type"
+    t.boolean "tagged"
+    t.string  "acceptable_frame_type"
+    t.string  "permission"
+    t.integer "ethernet_interface_id"
   end
 
-  create_table :bridge_vlans do |t|
-    t.column :vlan, :int
-    t.belongs_to :bridge_address_table
+  create_table "vlan_interfaces", force: true do |t|
+    t.string  "description"
+    t.integer "vlan"
+    t.integer "switch_id"
   end
 
-  create_table :macs do |t|
-    t.column :mac, :string
-    t.column :mode, :string
-    t.belongs_to :bridge_vlan
+  create_table "vlans", force: true do |t|
+    t.integer "vlan"
+    t.integer "switchport_id"
   end
 
-  execute <<-SQL
-    CREATE VIEW view_full AS
-      SELECT
-        networks.location AS location,
-        switches.hostname AS switch,
-        ethernet_interfaces.stack_member AS stack_member,
-        ethernet_interfaces.speed AS speed,
-        ethernet_interfaces.port AS port,
-        ethernet_interfaces.description AS port_description,
-        switchports.mode AS mode,
-        switchports.port_type AS port_type,
-        switchports.tagged AS tagged,
-        switchports.acceptable_frame_type AS acceptable_frame_type,
-        switchports.permission AS permission,
-        vlans.vlan AS vlan,
-        vlan_interfaces.description AS vlan_description,
-        macs.mac AS mac
-      FROM
-        switches
-      INNER JOIN networks              ON switches.network_id                  = networks.id
-      INNER JOIN ethernet_interfaces   ON ethernet_interfaces.switch_id        = switches.id
-      INNER JOIN switchports           ON switchports.ethernet_interface_id    = ethernet_interfaces.id
-      INNER JOIN vlans                 ON vlans.switchport_id                  = switchports.id
-      INNER JOIN vlan_interfaces       ON vlan_interfaces.vlan                 = vlans.vlan
-      INNER JOIN bridge_address_tables ON bridge_address_tables.switch_id      = switches.id
-      INNER JOIN bridge_vlans          ON bridge_vlans.bridge_address_table_id = bridge_address_tables.id
-      INNER JOIN macs                  ON macs.bridge_vlan_id                  = bridge_vlans.id
-  SQL
 end
