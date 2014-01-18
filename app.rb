35e2ff479e0fcb10671cfff2b31ace162c0ae98f
Rob Wilson <roobert@gmail.com>
2014-01-18 22:26:59 +0000

    removed data_mapper stuff

--- app.rb
+++ app.rb
@@ -12,20 +12,13 @@ require 'sqlite3'
 require 'awesome_print'
 require 'pp'
 require 'yaml'
-require 'switch_data_parser'
-require 'network_data_translator'
+require 'model'
 
 Sinatra::Application.reset!
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/db.sql')
+
 set :database, "sqlite3:///db/db.sql"
-
-DATA_DIR         = File.join(File.dirname(__FILE__), 'data')
-CONFIG_DIR       = File.join(DATA_DIR, 'config')
-BRIDGE_TABLE_DIR = File.join(DATA_DIR, 'bridge_address_table')
-
-require_relative './app/model.rb'
-require_relative './app/helpers.rb'
 
 def view_switch_full(switch)
   SwitchView.where("switch" => switch)
@@ -33,13 +26,6 @@ end
 
 def e(string)
   Rack::Utils.escape_html(string)
-end
-
-raise 'fatal error!' unless Network.table_exists?
-
-if Network.all.length == 0
-  load_data
-  translate_data
 end
 
 get '/css/:style.css' do
@@ -79,7 +65,6 @@ get '/:network/?' do
 
   haml :switches
 end
-
 
 get '/' do
   haml :networks
